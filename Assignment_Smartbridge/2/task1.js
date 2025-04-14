4d757575433a01ca7bd53d92f81b5266d062ad4b
komalbaid <140634863+komalbaid@users.noreply.github.com>
2025-04-14 20:22:22 +0530

    Delete Assignment_Smartbridge directory

--- Assignment_Smartbridge/2/task1.js
+++
@@ -1,6 +0,0 @@
-/*Declare a variable age and assign it a value of 25. Then, declare another variable is Adult that is true
-if age is 18 or more, otherwise false.*/
-
-let age = 25;
-let isAdult = age >= 18;
-console.log(isAdult);
--- Assignment_Smartbridge/2/task5.js
+++
@@ -1,8 +0,0 @@
-/*Write a function square that takes a number as input and
-returns its square.*/
-
-function square(number) {
-    return number * number;
-}
-let result = square(4);
-console.log(result);
